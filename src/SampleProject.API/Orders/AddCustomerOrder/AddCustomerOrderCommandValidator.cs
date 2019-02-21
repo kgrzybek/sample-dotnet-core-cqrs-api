96581f9a01fe4fc44db0a70f4bc7749947b6e83c
kgrzybek <kgrzybek@itsg.com.pl>
2019-02-21 01:46:40 +0100

    Add validation to AddCustomerOrderCommand handling. Add FluentValidation and Problem Details libraries.

---
+++ src/SampleProject.API/Orders/AddCustomerOrder/AddCustomerOrderCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace SampleProject.API.Orders.AddCustomerOrder
+{
+    public class AddCustomerOrderCommandValidator : AbstractValidator<AddCustomerOrderCommand>
+    {
+        public AddCustomerOrderCommandValidator()
+        {
+            RuleFor(x => x.CustomerId).NotEmpty().WithMessage("CustomerId is empty");
+            RuleFor(x => x.Products).NotEmpty().WithMessage("Products list is empty");
+            RuleForEach(x => x.Products).SetValidator(new ProductDtoValidator());
+        }
+    }
+}
