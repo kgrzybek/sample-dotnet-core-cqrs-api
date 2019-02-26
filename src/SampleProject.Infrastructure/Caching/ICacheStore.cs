581613054a581ac91e9430256b47fabde9b5634a
kgrzybek <kgrzybek@itsg.com.pl>
2019-02-26 16:48:31 +0100

    Add caching (cache-aside pattern)

---
+++ src/SampleProject.Infrastructure/Caching/ICacheStore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SampleProject.Infrastructure.Caching
+{
+    public interface ICacheStore
+    {
+        void Add<TItem>(TItem item, ICacheKey<TItem> key, TimeSpan? expirationTime = null);
+
+        TItem Get<TItem>(ICacheKey<TItem> key) where TItem : class;
+
+        void Remove<TItem>(ICacheKey<TItem> key);
+    }
+}
