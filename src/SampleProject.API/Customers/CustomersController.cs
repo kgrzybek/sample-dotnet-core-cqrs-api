0aff97e223b0e692dd053b51effcc6d466ff5fa2
Suat KÖSE <suadev@gmail.com>
2019-06-26 23:29:38 +0300

    refactoring for controllers

--- src/SampleProject.API/Customers/CustomersController.cs
+++ src/SampleProject.API/Customers/CustomersController.cs
@@ -1,5 +1,4 @@
-﻿using System.Net;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using SampleProject.API.Customers.RegisterCustomer;
@@ -8,7 +7,7 @@ namespace SampleProject.API.Customers
 {
     [Route("api/customers")]
     [ApiController]
-    public class CustomersController : Controller
+    public class CustomersController : ControllerBase
     {
         private readonly IMediator _mediator;
 
@@ -20,14 +19,12 @@ namespace SampleProject.API.Customers
         /// <summary>
         /// Register customer.
         /// </summary>
-        [Route("")]
         [HttpPost]
-        [ProducesResponseType(typeof(CustomerDto), (int)HttpStatusCode.Created)]
-        public async Task<IActionResult> RegisterCustomer([FromBody]RegisterCustomerRequest request)
+        public async Task<IActionResult> Post(RegisterCustomerRequest request)
         {
-           var customer = await _mediator.Send(new RegisterCustomerCommand(request.Email, request.Name));
+            var customer = await _mediator.Send(new RegisterCustomerCommand(request.Email, request.Name));
 
-           return Created(string.Empty, customer);
-        }       
+            return Created(string.Empty, customer);
+        }
     }
 }
